bb73f2089c4cd991c4ecae984530d57486289faf
mrdaulet <daulpakov@gmail.com>
2015-05-14 19:03:55 +0100

    Added Exercise 12

--- Exercise12_3/Program.cs
+++ Exercise12_3/Program.cs
@@ -10,17 +10,81 @@ namespace Exercise12_3
     {
         static void Main(string[] args)
         {
+            var invoiceA = new Invoice("A", 13);
+            var invoiceB = new Invoice("B", 18);
+            var invoiceA2 = new Invoice("A", 10);
+
+            Console.WriteLine("{0}, {1}, {2}", invoiceA, invoiceA2, invoiceB);
+            Console.WriteLine("{0} + {1} = {2}", invoiceA, invoiceA2, invoiceA + invoiceA2);
+            Console.WriteLine("{0} + {1} = {2}", invoiceA, invoiceB, invoiceA + invoiceB);
+
+            var invoiceACopy = new Invoice("A", 13);
+            Console.WriteLine("{0} == {1} is {2}", invoiceA, invoiceACopy, invoiceA == invoiceACopy);
+            Console.WriteLine("{0} == {1} is {2}", invoiceA, invoiceA2, invoiceA == invoiceA2);
+            Console.WriteLine("{0} == {1} is {2}", invoiceA, invoiceB, invoiceA == invoiceB);
+
+            invoiceB.amount = 13;
+            Console.WriteLine("{0} < {1} is {2}", invoiceA, invoiceA2, invoiceA < invoiceA2);
+            Console.WriteLine("{0} < {1} is {2}", invoiceA, invoiceB, invoiceA < invoiceB);
+            Console.WriteLine("{0} > {1} is {2}", invoiceA, invoiceA2, invoiceA > invoiceA2);
+            Console.WriteLine("{0} > {1} is {2}", invoiceA, invoiceB, invoiceA > invoiceB);
+
         }
     }
 
     class Invoice
     {
-        string vendor;
-        double amount;
+        public string vendor;
+        public double amount;
+
+        public Invoice(string vendor, double amount)
+        {
+            this.vendor = vendor;
+            this.amount = amount;
+        }
 
         public override string ToString()
         {
             return vendor + " " + amount;
+        }
+
+        public static double operator + (Invoice lhs, Invoice rhs)
+        {
+            if (lhs.vendor == rhs.vendor)
+            {
+                return lhs.amount + rhs.amount;
+            }
+
+            return 0;
+        }
+
+        public static bool operator == (Invoice lhs, Invoice rhs)
+        {
+            return lhs.vendor == rhs.vendor && lhs.amount == rhs.amount;
+        }
+
+        public static bool operator != (Invoice lhs, Invoice rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public static bool operator < (Invoice lhs, Invoice rhs)
+        {
+            return lhs.amount < rhs.amount;
+        }
+
+        public static bool operator > (Invoice lhs, Invoice rhs)
+        {
+            return lhs.amount > rhs.amount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Invoice))
+            {
+                return false;
+            }
+            return !(this == (Invoice)obj);
         }
     }
 }
---
+++ Exercise12_4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foot = new Foot(10);
+            var meter = new Meter(10);
+
+            Console.WriteLine("{0} = {1}", foot, (Meter)foot);
+            Console.WriteLine("{0} = {1}", meter, (Foot)meter);
+
+            Meter meter1 = foot;
+            Foot foot1 = meter;
+            Console.WriteLine(meter1);
+            Console.WriteLine(foot1);            
+        }
+    }
+
+    class Foot 
+    {
+        public double length;
+
+        public Foot(double length)
+        {
+            this.length = length;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} in foot measure", length);
+        }
+
+        public static implicit operator Foot(Meter meter) 
+        {
+            return new Foot(meter.length / 0.3048);
+        }
+
+        public static implicit operator Meter(Foot foot)
+        {
+            return new Meter(foot.length * 0.3048);
+        }
+    }
+
+    class Meter 
+    {
+        public double length;
+
+        public Meter(double length)
+        {
+            this.length = length;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} in normal measure", length);
+        }
+    }
+
+}
